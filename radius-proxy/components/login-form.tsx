b54959e263a2b70b9eb0d816ce18017ae5d5ac58
essinghigh <henry@essinghigh.dev>
2025-10-05 01:12:56 +0100

    Init

---
+++ radius-proxy/components/login-form.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-muted-foreground text-balance">
+                  Login to Grafana with your admin account
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="user">User</FieldLabel>
+                <Input
+                  id="user"
+                  type="text"
+                  placeholder="adm_example"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/grafana-logo.svg"
+              alt="Grafana Logo"
+              className="absolute inset-0 h-full w-full object-contain p-8 transform scale-50"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
